3406675e40ae60a3ff72b7eb1d92b391868cad89
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2012-03-14 19:43:16 +0900

    first

---
+++ adDeluxeSDK/adDeluxeSDK.m
@@ -0,0 +1,13 @@
+//
+//  adDeluxeSDK.m
+//  adDeluxeSDK
+//
+//  Created by Tatsuya Fukata on 3/14/12.
+//  Copyright (c) 2012 fukata.org. All rights reserved.
+//
+
+#import "adDeluxeSDK.h"
+
+@implementation adDeluxeSDK
+
+@end
